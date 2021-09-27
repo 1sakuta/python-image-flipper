22e82bdcb5d83540688fba74af3bec9347ea551b
Sakuta <347sakuta@list.ru>
2021-09-27 14:04:35 +0300

    Code

--- image-flipper.py
+++ image-flipper.py
@@ -1,1 +1,19 @@
-print('Hello GitHub!')
+#imort modules
+from PIL import Image
+import os
+
+#variable to open our image
+img = Image.open('./assets/rotateit.png')
+
+#code
+r_img = img.rotate(-90)
+r_img.save('./assets/rotated.png')
+os.remove('./assets/rotateit.png')
+r_img.show()
+
+#congratulation
+print('Your image was successfully rotated!')
+print('Now your new file name is rotated.png')
+
+#click enter button to close programm
+input()
